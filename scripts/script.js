b0f1aa096b866d29f366e24e309f4b24f744d87e
Eugene-Guryanov <103857729+Eugene-Guryanov@users.noreply.github.com>
2022-06-25 18:36:08 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const openPopup = document.querySelector(".profile__edit-button");
+const closePopup = document.querySelector(".popup__exit");
+let popup = document.querySelector(".popup");
+let name = document.getElementById("name");
+let description = document.getElementById("description");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__text_type_name");
+let jobInput = document.querySelector(".popup__text_type_description");
+
+function exit() {
+    popup.classList.remove("popup_status_active");
+}
+closePopup.addEventListener("click", exit);
+openPopup.addEventListener("click", function () {
+ popup.classList.add("popup_status_active");
+ nameInput.value = name.textContent;
+jobInput.value = description.textContent;
+});
+
+function formSubmitHandler (evt) {
+ evt.preventDefault();
+ name.textContent = nameInput.value;
+ description.textContent = jobInput.value; 
+ exit();
+}
+formElement.addEventListener("submit", formSubmitHandler)
