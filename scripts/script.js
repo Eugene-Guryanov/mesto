cf3483c6575d341e8158215898713002febe512a
Eugene-Guryanov <103857729+Eugene-Guryanov@users.noreply.github.com>
2022-06-27 17:24:54 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,11 @@ let nameInput = document.querySelector(".popup__text_type_name");
 let jobInput = document.querySelector(".popup__text_type_description");
 
 function exit() {
-    popup.classList.remove("popup_status_active");
+    popup.classList.remove("popup_type_edit");
 }
 closePopup.addEventListener("click", exit);
 openPopup.addEventListener("click", function () {
- popup.classList.add("popup_status_active");
+ popup.classList.add("popup_type_edit");
  nameInput.value = name.textContent;
 jobInput.value = description.textContent;
 });
