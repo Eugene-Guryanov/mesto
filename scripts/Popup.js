9e662e5703a19defd1508e7549d354df1529171f
Eugene-Guryanov <guryanov.e.s@gmail.com>
2023-01-02 21:06:03 +0300

    Выполнение 8 ПР.

---
+++ scripts/Popup.js
@@ -0,0 +1,9 @@
+export class Popup{
+    constructor (){
+
+    }
+    open(){}
+    close(){}
+    _handleEscClose(){}
+    setEventListeners(){}
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,28 @@
+import {
+  popupEditOpen, 
+  name,
+  description,
+  cardForm,
+  editForm,
+  nameInput,
+  jobInput,
+  popupCardOpen,
+  elementPageItem,
+  elementTemplate,
+  cardPopup,
+  editPopup,
+  nameTitle,
+  linkSource,
+  linkPopupImg,
+  titlePopupImg,
+  imagePopup,
+  initialCards,
+  components,
+} from "./Constants.js"
+
 import  {Card} from "./Card.js";
 import {FormValidation} from "./validate.js";
-const popupEditOpen = document.querySelector(".profile__edit-button");
-const name = document.getElementById("name");
-const description = document.getElementById("description");
-const cardForm = document.querySelector(".popup__form_type_add");
-const editForm = document.querySelector(".popup__form_type_edit");
-const nameInput = document.querySelector(".popup__input_type_name");
-const jobInput = document.querySelector(".popup__input_type_description");
-const popupCardOpen = document.querySelector(".profile__add-button");
-const elementPageItem = document.querySelector(".elements");
-const elementTemplate = document.querySelector(".element-template");
-const cardPopup = document.querySelector(".popup-add");
-const editPopup = document.querySelector(".popup-edit");
-const nameTitle = document.querySelector(".popup__input_type_title");
-const linkSource = document.querySelector(".popup__input_type_source");
-const linkPopupImg = document.querySelector(".popup__image");
-const titlePopupImg = document.querySelector(".popup__description");
-const imagePopup = document.querySelector(".popup-image");
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 
-const components = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-};
 
 
 
